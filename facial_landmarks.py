dcd013347798c0e982804b125f6065840f93b2bc
Conan1985 <Conan1985@users.noreply.github.com>
2020-02-14 21:52:34 -0500

    Add gitignore

--- facial_landmarks.py
+++ facial_landmarks.py
@@ -7,9 +7,9 @@ import cv2
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--shape-predictor", required=True,
-	help="path to facial landmark predictor")
+                help="path to facial landmark predictor")
 ap.add_argument("-i", "--image", required=True,
-	help="path to input image")
+                help="path to input image")
 args = vars(ap.parse_args())
 
 detector = dlib.get_frontal_face_detector()
@@ -24,21 +24,22 @@ rects = detector(gray, 1)
 totalFaces = 0
 
 for (i, rect) in enumerate(rects):
-	totalFaces += 1
+    totalFaces += 1
 
-	shape = predictor(gray, rect)
-	shape = face_utils.shape_to_np(shape)
+    shape = predictor(gray, rect)
+    shape = face_utils.shape_to_np(shape)
 
-	(x, y, w, h) = face_utils.rect_to_bb(rect)
-	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    (x, y, w, h) = face_utils.rect_to_bb(rect)
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-	cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-	for (x, y) in shape:
-		cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+    for (x, y) in shape:
+        cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
 
-if (totalFaces == 1):
-	cv2.imshow("Found 1 face", image)
-else: cv2.imshow("Found {} faces".format(totalFaces), image)
+if totalFaces == 1:
+    cv2.imshow("Found 1 face", image)
+else:
+    cv2.imshow("Found {} faces".format(totalFaces), image)
 cv2.waitKey(0)
